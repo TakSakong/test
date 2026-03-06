6efa4b2150924327b40626485134d6d413567037
TakSakong <k402900@naver.com>
2026-03-06 10:55:29 +0900

    topic select

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Welcome to My Development Journal API!"
+    return "빠르고 바쁜 현대인들을 위한 개인 맞춤형 뉴스 웹"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
